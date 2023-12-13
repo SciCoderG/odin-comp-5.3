0139e28aa2bd5c69aede477e2c1c4f62a2c133a2
David Liebemann <david@scico.dev>
2023-12-13 15:18:55 +0100

    Empty comp 5.3 project

---
+++ compatibility_5_3/Source/compatibility_5_3/compatibility_5_3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "compatibility_5_3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, compatibility_5_3, "compatibility_5_3" );
